c5574efc483e2c1358d39876abbc00d764dcc296
Nick <nsagias@gmail.com>
2021-09-23 03:33:13 -0400

    add say feature

--- input.js
+++ input.js
@@ -16,26 +16,16 @@ const handleUserInput = function(data, conn) {
   if(data === '\d') {
     conn.write('Move: right');
   }
+  if(data === '\g') {
+    conn.write("Say: I win");
+  }
+  
 };
 
 
 const setupInput = function(conn) {
   connection = conn;
   const stdin = process.stdin;
-  stdin.on('data', (data) => {
-    // if(data === '\w') {
-    //   conn.write('Move: up');
-    // }
-    // if(data === '\a') {
-    //   conn.write('Move: left');
-    // }
-    // if(data === '\s') {
-    //   conn.write('Move: down');
-    // }
-    // if(data === '\d') {
-    //   conn.write('Move: right');
-    // }
-  })
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.on('data', (data) => {
