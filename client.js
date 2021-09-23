c3e588348b8b81488349658a33e0540a359830b7
Nick <nsagias@gmail.com>
2021-09-22 22:27:00 -0400

    Move up, set Interval and setTimeout examples

--- client.js
+++ client.js
@@ -3,8 +3,10 @@ const net = require("net");
 // establishes a connection with the game server
 const connect = function() {
   const conn = net.createConnection({
-    host: '135.23.223.133',// IP address here,
-    port: 50542 // PORT number here,
+    // host: '135.23.223.133',// IP address here,
+    // port: 50542 // PORT number here,
+    host: 'localhost',// IP address here,
+    port: 50541 // PORT number here,
   });
 
   // interpret incoming data as text
@@ -13,8 +15,37 @@ const connect = function() {
   conn.on('connect', () => {
     conn.write('Name: NNN');
   });
-   
+  conn.on('connect', () => {
+    conn.write('Move: up'); 
+  });
+  
+  // "Move: up" - move up one square (unless facing down)
+  // "Move: down" - move down one square (unless facing up)
+  // "Move: left" - move left one square (unless facing right)
+  // "Move: right" - move left one square (unless facing left)
+  
+  let moveArray = ["Move: up", "Move: down", "Move: left", "Move: right"];
+  
+  // conn.on('connect', () => {
+  //   conn.write('Move: up'); 
+  //   for (let i = 0; i < moveArray.length; i++) {
+  //     const waitTime = 50 * (i + 1);
+  //     setTimeout(()=> {
+  //       conn.write(moveArray[0]);
+  //     }, waitTime);
+  //   }
   // });
+  // conn.on('connect', () => {
+  //     conn.write('Move: up'); 
+  //     for (let i = 0; i < moveArray.length; i++) {
+  //       const waitTime = 50 * (i + 1);
+  //       setInterval(()=> {
+  //         conn.write(moveArray[0]);
+  //       }, waitTime);
+  //     }
+  //   });
+  
+ 
   conn.on('data', (data) => {
     console.log(data.toString());
   });
